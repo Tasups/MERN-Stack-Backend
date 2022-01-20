1873684247856d806d9098f4cc562918eaa40f4a
Jason Whisnant <tasups@gmail.com>
2022-01-20 15:44:05 -0500

    google maps geolocation fully integrated on backend

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -53,13 +53,23 @@ const getPlacesByUserId = (req, res, next) => {
   res.json({ places });
 };
 
-const createPlace = (req, res, next) => {
+const createPlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     console.log(errors)
-    throw new HttpError('Invalid information, please check and try again.', 422)
+    return next(
+      new HttpError("Invalid information, please check and try again.", 422)
+    );
   }
-  const { title, description, coordinates, address, creator } = req.body;
+  const { title, description, address, creator } = req.body;
+
+  let coordinates;
+  try {
+    coordinates = await getCoordsForAddress(address);
+  } catch (error) {
+    return next(error);
+  }
+    
   const createdPlace = {
     id: nanoid(),
     title,
